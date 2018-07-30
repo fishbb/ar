416d2b93bdce2916191bc13339ca186ca89ba028
Dan Lou <loudan1980@gmail.com>
2018-07-30 12:00:36 -0700

    change marker

--- aframe-ar.js
+++ aframe-ar.js
@@ -5660,10 +5660,9 @@ ARjs.Profile.prototype.reset = function () {
 	}
 	this.defaultMarkerParameters = {
 		type : 'pattern',
-// 		patternUrl : THREEx.ArToolkitContext.baseURL + '../data/data/patt.hiro',
-// 		changeMatrixMode: 'modelViewMatrix',
-        patternUrl : THREEx.ArToolkitContext.danlouURL + 'marker/robot.patt'
-// 		changeMatrixMode: 'modelViewMatrix',
+		patternUrl : THREEx.ArToolkitContext.baseURL + '../data/data/patt.hiro',
+		changeMatrixMode: 'modelViewMatrix',
+//         patternUrl : THREEx.ArToolkitContext.danlouURL + 'marker/robot.patt'
 	}
 	return this
 };
